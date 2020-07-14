14db9b3a38ceb98ac2e27a78481ba218db6e8192
Mitty <mitty.fun@gmail.com>
2020-07-14 10:20:03 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const { parallel, src, dest, watch } = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const webserver = require('gulp-webserver');
+const browserify = require('gulp-browserify');
+
+sass.compiler = require('node-sass');
+
+function html() {
+  return src('src/pug/*.pug')
+    .pipe(pug())
+    .pipe(dest('dist/html/'));
+}
+
+function javascript() {
+  return src('src/js/*.js')
+    .pipe(browserify())
+    .pipe(dest('dist/js/'));
+}
+
+function css() {
+  return src('src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css/'));
+}
+
+function server() {
+  return src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+};
+
+function copyAssets () {
+  return src('./static/*')
+    .pipe(dest('dist/static/'))
+
+}
+exports.default = function() {
+  
+  watch('src/js/*.js', javascript);
+  watch('src/scss/*.scss', css);
+  watch(['src/pug/*.pug', 'src/pug/*/*.pug'], html);
+
+  parallel(html, javascript, css, server, copyAssets)();
+};
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper-container', {
+    loop: true,
+})
+
+
+// $(document).ready(function () {
+//     $('.js-slick').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+// });
